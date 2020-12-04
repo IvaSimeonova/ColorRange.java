19bc97f4ff5b23eb3008115a78fa1d1fc0b0fe7b
IvaSimeonova <72191568+IvaSimeonova@users.noreply.github.com>
2020-12-04 14:59:15 +0200

    Add files via upload

--- Account.java
+++ Account.java
@@ -1,5 +1,63 @@
 package Iva;
 
-public class Account {
+import java.util.Date;
 
+public class Account {
+	private static double yearInterestRate = 4.5;
+	
+	private int id = 0;
+	private double balance = 0;
+	private Date dateCreated = new Date();
+	
+	Account(){
+	}
+	
+	Account( int id, double balance){
+		this.setId(id);
+		this.setBalance(balance);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getBalance() {
+		return balance;
+	}
+
+	public void setBalance(double balance) {
+		this.balance = balance;
+	}
+
+	public Date getDateCreated() {
+		return dateCreated;
+	}
+
+	public static double getYearInterestRate() {
+		return yearInterestRate;
+	}
+
+	public static void setYearInterestRate(double yearInterestRate) {
+		Account.yearInterestRate = yearInterestRate;
+	}
+	
+	public static double getMonthlyInterestRate(){
+		return yearInterestRate / 12;
+	}
+	
+	public double getMonthlyInterest(){
+		return balance*yearInterestRate / 12 / 100;
+	}
+	
+	public void withdraw(double draw){
+		balance -= draw;
+	}
+	
+	public void deposit(double sum){
+		balance += sum;
+	}
 }
--- AccountTest.java
+++ AccountTest.java
@@ -4,7 +4,10 @@ public class AccountTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		Account account = new Account(1222, 20000);
+		account.withdraw(2500);
+		account.deposit(3000);
+        System.out.println("Balance " + account.getBalance() + "\nMonthly interest " + account.getMonthlyInterest() + "\nDate created " + account.getDateCreated());
 	}
 
 }

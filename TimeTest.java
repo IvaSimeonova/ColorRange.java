2a23fbddfef7f5932c9fbd9d06775776a39e7c21
IvaSimeonova <72191568+IvaSimeonova@users.noreply.github.com>
2020-12-18 09:32:56 +0200

    Update TimeTest.java

--- TimeTest.java
+++ TimeTest.java
@@ -4,6 +4,8 @@ public class TimeTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+                Time time = new Time();
+		System.out.println(time.toString());
 
 	}
 
